56ee3640185a566f9e0561c19aabaa450731c7df
Felix Morgner <felix.morgner@gmail.com>
2011-04-14 16:46:38 +0200

    added PreferenceController class which will be used for the preferences window

---
+++ Source/PreferencesController.h
@@ -0,0 +1,17 @@
+//
+//  PreferencesController.h
+//  FlickrLinks
+//
+//  Created by Felix Morgner on 14.04.11.
+//  Copyright 2011 Bühler AG. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PreferencesController : NSWindowController {
+@private
+    
+}
+
+@end
---
+++ Source/PreferencesController.m
@@ -0,0 +1,36 @@
+//
+//  PreferencesController.m
+//  FlickrLinks
+//
+//  Created by Felix Morgner on 14.04.11.
+//  Copyright 2011 Bühler AG. All rights reserved.
+//
+
+#import "PreferencesController.h"
+
+
+@implementation PreferencesController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
